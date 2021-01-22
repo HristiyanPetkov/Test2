7ee5258e4eb23691e68dbac7e8a6d8a22cd09051
Hrisitqn Petkov <71525781+HristiyanPetkov@users.noreply.github.com>
2021-01-22 13:35:45 +0200

    Update search_text.py

--- search_text.py
+++ search_text.py
@@ -3,8 +3,6 @@ def search_and_replace_text(search_sentence, text):
     
     if ''.join(search_sentence) not in text:
         return "N/A"
-    elif len(search_sentence) < 2:
-        return "N/A"
     
     text = text.split(' ')
     search_sentence = "".join(search_sentence)
