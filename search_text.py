f0f14401509dbb0e0a630a873ed8d6eebd7fbb52
Hrisitqn Petkov <71525781+HristiyanPetkov@users.noreply.github.com>
2021-01-22 13:27:56 +0200

    Update search_text.py

--- search_text.py
+++ search_text.py
@@ -7,7 +7,7 @@ def search_and_replace_text(search_sentence, text):
         return "N/A"
     
     text = text.split(' ')
-    search_sentence = " ".join(search_sentence)
+    search_sentence = "".join(search_sentence)
     text.append(search_sentence)
     text = ' '.join(text)
     text = text.split(search_sentence)
