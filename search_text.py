174f6d290186893ab9163017181985f4efa8cbf6
Hrisitqn Petkov <71525781+HristiyanPetkov@users.noreply.github.com>
2021-01-22 13:42:20 +0200

    Update search_text.py

--- search_text.py
+++ search_text.py
@@ -6,4 +6,12 @@ def search_and_replace_text(search_sentence, text):
     elif len(search_sentence) < 2:
         return "N/A"
     
+    text = text.split(' ')
+    search_sentence = "".join(search_sentence)
+    text.append(search_sentence)
+    text = ' '.join(text)
+    text = text.split(search_sentence)
+    text.append(search_sentence)
+    text = "".join(text)
+    
     return text
