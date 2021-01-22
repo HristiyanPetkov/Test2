62e3fa7a21dfc59f314b97c91cd1cf216ee411e1
Hrisitqn Petkov <71525781+HristiyanPetkov@users.noreply.github.com>
2021-01-22 13:51:48 +0200

    Update search_text.py

--- search_text.py
+++ search_text.py
@@ -4,12 +4,8 @@ def search_and_replace_text(search_sentence, text):
     if ''.join(search_sentence) not in text:
         return "N/A"
     
-    text = text.split(' ')
     search_sentence = "".join(search_sentence)
-    text.append(search_sentence)
-    text = ' '.join(text)
-    text = text.split(search_sentence)
-    text.append(search_sentence)
-    text = "".join(text)
+    text = text.replace(search_sentence, '')
+    text += search_sentence
     
     return text
