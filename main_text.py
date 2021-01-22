11a75cc1b7a7d880d4a56a458f337b7e7984e139
Hrisitqn Petkov <71525781+HristiyanPetkov@users.noreply.github.com>
2021-01-22 13:38:26 +0200

    Update main_text.py

--- main_text.py
+++ main_text.py
@@ -1,6 +1,6 @@
 from search_text import search_and_replace_text
 
-text = "This is a text longer than two sentences and this is the first one. This the second sentence."
+text = "This is a text longer than two sentences and this is the first one. This is the second sentence."
 print(text)
 phrase = input("What phrase do you want to search for.")
 
